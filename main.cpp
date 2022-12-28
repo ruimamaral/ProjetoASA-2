00413246dbe9c539978cc2415e537b5f3643929f
someonearound <ruimiguel.amaral@hotmail.com>
2022-12-28 22:43:51 +0000

    fixed more things

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,12 +14,14 @@ typedef struct {
 
 struct cmp_edges {
 	bool operator() (const edge &a, const edge &b) const {
-		return a.weight < b.weight;
+		return a.weight > b.weight;
 	}
 };
 
 int get_head(int v, vector<int> &head) {
-	int p = head[v], pp = head[p];
+	int p = head[v],
+		pp = head[p];
+	printf("%d, %d cona \n", v, p);
 	if (pp != p) {
 		head[v] = get_head(pp, head);
 		head[p] = head[v];
@@ -37,12 +40,14 @@ void unite(int u, int v, vector<int> &head, vector<int> &rank) {
 	}
 }
 
-int kruskal(vector<int> &head, vector<int> &rank, set<edge, cmp_edges> &edges) {
-	set<edge>::iterator iter;
+int kruskal(vector<int> &head, vector<int> &rank, vector<edge> &edges) {
+	vector<edge>::iterator iter;
 	int weight = 0;
 	for (iter = edges.begin(); iter != edges.end(); iter++) {
+		printf("%d, %d, %d alive\n", iter->u, iter->v, iter->weight);
 		int u = iter->u;
 		int v = iter->v;
+		printf("%d %d banana \n", u, v);
 		if (get_head(u, head) != get_head(v, head)) {
 			unite(u, v, head, rank);
 			weight += iter->weight;
@@ -56,10 +61,11 @@ int main() {
 
 	cin >> vs;
 	cin >> e;
+	printf("%d, %d stop\n", e, vs);
 
-	vector<int> heads(vs);
-	vector<int> rank(vs);
-	set<edge, cmp_edges> edges;
+	vector<int> heads(vs + 1);
+	vector<int> rank(vs + 1);
+	vector<edge> edges(e);
 
 	for (int i = 0; i < e; i++) {
 		edge *new_edge;
@@ -68,13 +74,25 @@ int main() {
 		cin >> v;
 		cin >> w;
 		new_edge = new edge {u, v, w};
-		edges.insert((*new_edge));
+		edges[i] = ((*new_edge));
+		printf("%d, %d, %d\n", new_edge->u, new_edge->v, new_edge->weight);
 	}
-	for (int i = 0; i < vs; i++) {
+
+	sort(edges.begin(), edges.end(), cmp_edges());
+
+	for (int i = 0; i < vs + 1; i++) {
 		heads[i] = i;
 		rank[i] = 0;
 	}
+	for (int i = 0; i < vs + 1; i++) {
+		cout << heads[i];
+		cout << rank[i] << "\n";
+	}
+	vector<edge>::iterator iter;
+	for (iter = edges.begin(); iter != edges.end(); iter++) {
+		printf("%d, %d, %d here\n", iter->u, iter->v, iter->weight);
+	}
 
-	cout << kruskal(heads, rank, edges) << "\n";
+	cout << kruskal(heads, rank, edges) << " gay\n";
 	return 0;
 }
