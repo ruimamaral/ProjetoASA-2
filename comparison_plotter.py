8990c00b1752d21b50a46231ce1876f21ceb6f87
someonearound <ruimiguel.amaral@hotmail.com>
2023-01-05 22:33:19 +0000

    change spaces to tabs (have to fix nvim config later)

--- comparison_plotter.py
+++ comparison_plotter.py
@@ -5,22 +5,22 @@ import matplotlib.pyplot as plt
 from scipy.special import lambertw
 
 df = pd.DataFrame([
-    [1000000, 160],
-    [2000000, 300],
-    [3000000, 450],
-    [4000000, 620],
-    [5000000, 780],
-    [6000000, 940],
-    [7000000, 1080],
-    [8000000, 1220],
-    [9000000, 1400],
-    [10000000, 1550],
-    [11000000, 1710],
-    [12000000, 1880],
-    [13000000, 2020],
-    [14000000, 2230],
-    [15000000, 2330]],
-    columns=["edges", "time"])
+	[1000000, 160],
+	[2000000, 300],
+	[3000000, 450],
+	[4000000, 620],
+	[5000000, 780],
+	[6000000, 940],
+	[7000000, 1080],
+	[8000000, 1220],
+	[9000000, 1400],
+	[10000000, 1550],
+	[11000000, 1710],
+	[12000000, 1880],
+	[13000000, 2020],
+	[14000000, 2230],
+	[15000000, 2330]],
+	columns=["edges", "time"])
 
 sns.set(rc={'figure.figsize':(10,6)})
 plot = sns.lineplot(x='edges', y='time', data=df, marker="o", c="blue", label='escala normal')
@@ -30,11 +30,11 @@ ax2 = plt.twiny()
 ax2 = sns.lineplot(x='edges', y='time', data=df, marker="o", c="red", ax=ax2, label='escala E*log(E)')
 
 def forward(x):
-    with np.errstate(divide='ignore', invalid='ignore'):
-        return x * np.log(x) 
+	with np.errstate(divide='ignore', invalid='ignore'):
+		return x * np.log(x) 
 
 def inverse(x):
-    return x / lambertw(x)
+	return x / lambertw(x)
 
 ax2.set_xscale("function", functions=(forward, inverse))
 ax2.set_xlabel("Arestas")
--- plotter.py
+++ plotter.py
@@ -4,22 +4,22 @@ import numpy as np
 from scipy.special import lambertw
 
 df = pd.DataFrame([
-    [1000000, 160],
-    [2000000, 300],
-    [3000000, 450],
-    [4000000, 620],
-    [5000000, 780],
-    [6000000, 940],
-    [7000000, 1080],
-    [8000000, 1220],
-    [9000000, 1400],
-    [10000000, 1550],
-    [11000000, 1710],
-    [12000000, 1880],
-    [13000000, 2020],
-    [14000000, 2230],
-    [15000000, 2330]],
-    columns=["edges", "time"])
+	[1000000, 160],
+	[2000000, 300],
+	[3000000, 450],
+	[4000000, 620],
+	[5000000, 780],
+	[6000000, 940],
+	[7000000, 1080],
+	[8000000, 1220],
+	[9000000, 1400],
+	[10000000, 1550],
+	[11000000, 1710],
+	[12000000, 1880],
+	[13000000, 2020],
+	[14000000, 2230],
+	[15000000, 2330]],
+	columns=["edges", "time"])
 
 sns.set(rc={'figure.figsize':(10,6)})
 plot = sns.lineplot(x='edges', y='time', data=df, marker="o")
@@ -29,11 +29,11 @@ plot.set_title("Complexidade Temporal")
 plot.figure.savefig("ct1.png")
 
 def forward(x):
-    with np.errstate(divide='ignore', invalid='ignore'):
-        return x * np.log(x) 
+	with np.errstate(divide='ignore', invalid='ignore'):
+		return x * np.log(x) 
 
 def inverse(x):
-    return x / lambertw(x)
+	return x / lambertw(x)
 
 plot.set_xscale("function", functions=(forward, inverse))
 plot.set_title("Complexidade Temporal Com Escala Ajustada")
