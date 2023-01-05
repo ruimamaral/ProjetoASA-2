fdf890f9d023fabd0ea3753a10df0da6b44be72f
someonearound <ruimiguel.amaral@hotmail.com>
2023-01-05 22:29:28 +0000

    add line comparison plotter

--- comparison_plotter.py
+++ comparison_plotter.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.special import lambertw
 
 df = pd.DataFrame([
@@ -22,11 +23,11 @@ df = pd.DataFrame([
     columns=["edges", "time"])
 
 sns.set(rc={'figure.figsize':(10,6)})
-plot = sns.lineplot(x='edges', y='time', data=df, marker="o")
-plot.set_xlabel("Arestas")
-plot.set_ylabel("Tempo (ms)")
-plot.set_title("Complexidade Temporal")
-plot.figure.savefig("ct1.png")
+plot = sns.lineplot(x='edges', y='time', data=df, marker="o", c="blue", label='escala normal')
+
+ax2 = plt.twiny()
+
+ax2 = sns.lineplot(x='edges', y='time', data=df, marker="o", c="red", ax=ax2, label='escala E*log(E)')
 
 def forward(x):
     with np.errstate(divide='ignore', invalid='ignore'):
@@ -35,6 +36,16 @@ def forward(x):
 def inverse(x):
     return x / lambertw(x)
 
-plot.set_xscale("function", functions=(forward, inverse))
-plot.set_title("Complexidade Temporal Com Escala Ajustada")
-plot.figure.savefig("ct2.png")
+ax2.set_xscale("function", functions=(forward, inverse))
+ax2.set_xlabel("Arestas")
+plot.set_xlabel("Arestas")
+plot.set_ylabel("Tempo (ms)")
+plot.set_title("Complexidade Temporal (Comparação)")
+
+h1, l1 = plot.get_legend_handles_labels()
+h2, l2 = ax2.get_legend_handles_labels()
+ax2.legend(h2 + h1, l2 + l1, loc='upper left')
+plot.get_legend().remove()
+
+plot.figure.savefig("ct3.png")
+
